fac79b8175694fcfe1df3f7332ecb4b9207bb771
Thomas Daley <thomas@volo.net>
2019-04-12 12:06:33 -0500

    Update paypal so city value is actually changed

--- testcallcenter.php
+++ testcallcenter.php
@@ -837,7 +837,8 @@
 
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script> 
 <script type="text/javascript" src="testscreens.js"></script>
 <script type="text/javascript" src="testcallcenter.js"></script>
 <script type="text/javascript" src="jquery.autocomplete.js"></script>
